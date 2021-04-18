1b5a1ef0ae38a905174a7e463b917988a4e7ef1f
Lavis Winston <bachtran.2000@gmail.com>
2021-04-18 11:13:55 +0700

    fix rmi qlsv

--- src/kiemTra/RMI_QLSV/server_RMI.java
+++ src/kiemTra/RMI_QLSV/server_RMI.java
@@ -49,8 +49,7 @@ public class server_RMI extends UnicastRemoteObject implements inter_RMI {
         }
         fw.close();
     }
-
-
+    
     @Override
     public String showAll() throws RemoteException {
         String result = "";
