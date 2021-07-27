ed60fc8772434a40738b346c1506e9899879e8eb
Trần Cao Minh Bách <45133811+bachtran2000@users.noreply.github.com>
2021-07-27 16:42:18 +0700

    Complete de le mien Nam

--- src/de_le_mien_nam/Client_FTP.java
+++ src/de_le_mien_nam/Client_FTP.java
@@ -5,18 +5,36 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client_FTP {
+    public static void WriteFile(String data, String fileName) throws IOException {
+        File f = new File("");
+        String preFix_path = f.getAbsolutePath() + "/src/de_le_mien_nam/Clientfile/";
+
+        f = new File(preFix_path + fileName);
+
+        FileWriter fw = new FileWriter(f);
+        fw.write(data);
+        fw.close();
+        System.out.println("Ghi file " + fileName + " thanh cong!");
+    }
+
     public static void main(String[] args) throws IOException {
         Socket cl = new Socket("localhost", 2349);
 
         DataInputStream dis = new DataInputStream(cl.getInputStream());
         DataOutputStream dos = new DataOutputStream(cl.getOutputStream());
 
-        ObjectOutputStream oos=new ObjectOutputStream(cl.getOutputStream());
-        ObjectInputStream ois=new ObjectInputStream(cl.getInputStream());
-
+        //nhap ten file, trong vi du la test.txt
         System.out.println("Nhap vao ten file can tim: ");
-        dos.writeUTF(new Scanner(System.in).nextLine());
+        String fileName = new Scanner(System.in).nextLine();
+        dos.writeUTF(fileName);
 
+        //Doc va xuat len man hinh data server tra lai
+        String data = dis.readUTF();
 
+        //Xac dinh co null khong thi ghi vao file o folder Clientfile
+        if (!data.equalsIgnoreCase("Khong ton tai file!")) {
+            System.out.println("Noi dung trong file " + fileName + ": \n" + data);
+            WriteFile(data, fileName);
+        } else System.out.println(data);
     }
 }
--- src/de_le_mien_nam/Server_FTP.java
+++ src/de_le_mien_nam/Server_FTP.java
@@ -5,24 +5,36 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server_FTP {
-    public static void ReadFile(String fileName) throws IOException {
-        File f = new File(fileName);
-        FileReader fr = new FileReader(f);
-        BufferedReader br = new BufferedReader(fr);
+    public static String ReadFile(String fileName) throws IOException {
+        File f = new File("");
+        String preFix_path = f.getAbsolutePath()+"/src/de_le_mien_nam/Serverfile/";
 
-        String line;
-        while ((line = br.readLine()) != null) {
-
-        }
+        f = new File(preFix_path+fileName);
+        if (f.exists()) {
+            FileReader fr = new FileReader(f);
+            BufferedReader br = new BufferedReader(fr);
+            String result = "";
+            String line;
+            while ((line = br.readLine()) != null) {
+                result += line + "\n";
+            }
+            fr.close();
+            br.close();
+            return result;
+        } else return "Khong ton tai file!";
     }
 
     public static void main(String[] args) throws IOException {
+
         ServerSocket server = new ServerSocket(2349);
         Socket my_cl = server.accept();
         DataInputStream dis = new DataInputStream(my_cl.getInputStream());
-        ObjectOutputStream oos = new ObjectOutputStream(my_cl.getOutputStream());
-        ObjectInputStream ois = new ObjectInputStream(my_cl.getInputStream());
+        DataOutputStream dos = new DataOutputStream(my_cl.getOutputStream());
 
+        String fileName = dis.readUTF();
+
+        dos.writeUTF(ReadFile(fileName));
 
     }
 }
+
